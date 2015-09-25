37100b652ac1ea9f11d005c8980bec721d881e67
Kornwika Kongruan <561997003@crru.ac.th>
2015-09-25 14:50:45 +0700

    new

--- array.java
+++ array.java
@@ -1,17 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class array {
   public static void main (String args[]) {
         Scanner in =new Scanner(System.in);
         System.out.println("arr:");
-      int[]a =new int[8];
+      int[]a =new int[9];
        String  ce=in.next();
-    for (i=0;i< a.length();i++){
-a[i]=Character.getNamericValue((ce.cherAt(i)));
+    for (int i=0;i< ce.length();i++){
+a[i]=Character.getNumericValue((ce.charAt(i)));
       
       // Assign Value to A
       
-     System.out.println(Arrays.toString[a]); 
+System.out.print(Arrays.toString(a));
 // Print a Array
+
+  }
 
   }
 }
