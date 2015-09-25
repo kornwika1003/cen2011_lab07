b561c5ebbc4fb572511ba1327b3000623ad707cd
Kornwika Kongruan <561997003@crru.ac.th>
2015-09-25 14:27:09 +0700

    ttt

---
+++ array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class array {
+  public static void main (String args[]) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("arr:");
+      int[]a =new int[8];
+       String  ce=in.next();
+    for (i=0;i< a.length();i++){
+a[i]=Character.getNamericValue((ce.cherAt(i)));
+      
+      // Assign Value to A
+      
+     System.out.println(Arrays.toString[a]); 
+// Print a Array
+
+  }
+}
